5f03506a179c937e7d90d34990065135d69ea68a
murderousegg <f.w.overbeeke@student.tue.nl>
2022-05-12 14:08:58 +0200

    Nieuw bestand waarmee je beter kan zien hoe 1 entry in de json files eruit ziet, en waarmee je kan expirimenteren in de console met bepaalde kolommen extracten

---
+++ analyse_data.py
@@ -0,0 +1,23 @@
+
+#Deze code can je in de python console (onderaan)
+#copy pasten en runnen
+#je krijgt dan rechts de variabelen te zien die overal zitten
+#van 1 rij van 1 file
+#NOTE: pas wel aan in welke directory je zit!
+#gebruik : 'C:\\map\\map\\map\\file.json'
+#HET VOORDEEL is dat je makkelijk de structuur van 1 entry
+#kan zien, en nested dictionaries makkelijk kan lezen
+#gebruik bv de console om doc['entities']['user_mentions']
+#te extracten, of andere soortgelijke dingen
+#dan kan je ze later toevoegen aan de efficient loading ding
+import pandas as pd
+import json
+data=[]
+
+cols = ['id_str', 'truncated']
+with open('D:\\data\\airlines-1558527599826.json', encoding='latin-1') as f:
+    for line in f:
+        doc = json.loads(line)
+        lst = [doc['id_str'], doc['truncated']]
+        data.append(lst)
+dftemp = pd.DataFrame(data=data, columns=cols)
