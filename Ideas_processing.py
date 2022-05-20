b7e981b904a5f03b3b1680baa2e80075c3da31cd
20161854 <f.w.overbeeke@student.tue.nl>
2022-05-20 15:58:36 +0200

    Ideeen en concerns toegevoegd onderaan Ideas_processing.py over het maken van een tabel met de heads en tails van conversaties

--- Ideas_processing.py
+++ Ideas_processing.py
@@ -28,3 +28,41 @@ if doc['truncated'] == True:
 
 
 if doc['in_reply_to_user_id_str'] != NULL:
+
+
+#CODE VOOR DATE ALS STRING OMZETTEN NAAR DATETIME OBJ ZODAT JE KAN SUBSTRACTEN
+from datetime import datetime
+date = datetime.strptime(doc['created_at'], "%a %b %d %H:%M:%S %z %Y")
+
+#PROBLEEM: "HEAD" EN TAIL VINDEN VAN CONVERSATION
+#VOOR DE TAIL IS EEN QUERY VOLDOENDE IN DE REPLIES TABLE:
+SELECT id_str as tail_id
+from replies
+where id_str NOT IN  (select in_reply_to_status_id_str from replies);
+
+
+#EXTRACTING CONVERSATIONS
+#VOOR DE HEAD HEB IK GEEN CONCREET IDEE MAAR
+tail_id.in_reply_to_status_id_str=temp_head
+for temp_head in replies.id_str:
+    temp_head=temp_head.in_reply_to_status_id_str
+head_id=temp_head
+#geen idee hoe je dit implement in sql ofzo, maar het idee is
+#je pakt de tweet waarop tail_id antwoord, maakt die de tijdelijke head
+#zoekt die id op in replies.id_str, en pakt de reply daarop,
+#maakt die nieuwe head. laat loopen totdat temp_head niet meer
+#in id_str staat, oftewel totdat er geen tweet meer is die er op replied.
+#vervolgens wil je head_id en tail_id in een sqlite table zetten.
+#MOGELIJKE PROBLEMEN: uiteindelijk willen we natuurlijk sentiment
+#analysis doen op de niet KLM tweets. Dus je moet eigenlijk de eerste en laatste
+#niet KLM tweet pakken. Daarbovenop moet je een counter toevoegen die bijhoudt
+#of je eindresultaat voldoet aan de lengte van een conversation.
+#hoe check je of je conversatie tussen klm en 1 klant is, en niet klanten onderling?
+#onze data gaat maar over een jaar, het kan zijn dat tweets antwoorden zijn op tweets die we
+#niet hebben?/dat er nog tweets antwoorden op onze tweets, maar de antwoorden niet hebben?
+#MOGELIJKE OPLOSSINGEN: sla in de for loop twee temp values op, zodat als
+#je head klm blijkt te zijn, je de id van daarvoor kan pakken. WEL EEN IDENTIFIER AANPLAKKEN
+#ZODAT WE WETEN DAT HET EEN CONVO IS DIE KLM START!!!
+#we zouden evt een query kunnen doen die alle tweets droppen die niet van
+#KLM zijn of waar KLM niet op gereplied wordt. Je mist dan alleen wel structuur:
+#user1<KLM<user2<KLM en uberhaupt user<KLM
