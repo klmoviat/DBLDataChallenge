43546c263e1a8daac384d0d2b494ba4ed01a81cd
murderousegg <f.w.overbeeke@student.tue.nl>
2022-06-16 12:46:39 +0200

    dpi toegevoegd, en wat dingen aangepast aan violinplots

--- def_plots.py
+++ def_plots.py
@@ -32,6 +32,7 @@ def avg_bar_plot(conn, cursor):
     plt.title('Average sentiment per company over the year')
     plt.tight_layout()
     plt.grid()
+    plt.savefig("Plots\\AVG_bar.png", format='png')
     plt.show()
 
 
@@ -57,7 +58,7 @@ def violin_sentiment(conn, cursor):
         " delta_sentiment from Lufthansa",
         conn)
 
-    fig = plt.figure(figsize=(12, 6))
+    fig = plt.figure(figsize=(12, 6), dpi=1200)
     gs = fig.add_gridspec(2, 6)
 
     ax = fig.add_subplot(gs[0, 0])
@@ -67,22 +68,22 @@ def violin_sentiment(conn, cursor):
     ax = fig.add_subplot(gs[0, 1])
     sns.violinplot(data=df1['last_tweet'], inner="quartile")
     ax.set_xlabel("last_tweet")
-
+    ax.set_title('British Airways')
     ax = fig.add_subplot(gs[0, 2])
     sns.violinplot(data=df1['delta_sentiment'], inner="quartile")
     ax.set_xlabel("delta_sentiment")
-    plt.title('Distribution of opening, closing and delta sentiment for British Airways (top-left), KLM (top right),'
-              'RyanAir (bottom-left), and Lufthansa (bottom-right) ')
+
     ax = fig.add_subplot(gs[0, 3])
-    sns.violinplot(data=df2['opening_tweet'], color='forestgreen', inner="quartile")
+    sns.violinplot(data=df2['opening_tweet'], color='darkorange', inner="quartile")
     ax.set_xlabel("opening_tweet")
 
     ax = fig.add_subplot(gs[0, 4])
-    sns.violinplot(data=df2['last_tweet'], color='forestgreen', inner="quartile")
+    sns.violinplot(data=df2['last_tweet'], color='darkorange', inner="quartile")
     ax.set_xlabel("last_tweet")
+    ax.set_title('KLM')
 
     ax = fig.add_subplot(gs[0, 5])
-    sns.violinplot(data=df2['delta_sentiment'], color='forestgreen', inner="quartile")
+    sns.violinplot(data=df2['delta_sentiment'], color='darkorange', inner="quartile")
     ax.set_xlabel("delta_sentiment")
 
     ax = fig.add_subplot(gs[1, 0])
@@ -92,11 +93,10 @@ def violin_sentiment(conn, cursor):
     ax = fig.add_subplot(gs[1, 1])
     sns.violinplot(data=df3['last_tweet'], color='gold', inner="quartile")
     ax.set_xlabel("last_tweet")
-
+    ax.set_title('RyanAir')
     ax = fig.add_subplot(gs[1, 2])
     sns.violinplot(data=df3['delta_sentiment'], color='gold', inner="quartile")
     ax.set_xlabel("delta_sentiment")
-    plt.title('Distribution of opening, closing and delta sentiment for RyanAir (left) and Lufthansa (right)')
     ax = fig.add_subplot(gs[1, 3])
     sns.violinplot(data=df4['opening_tweet'], color='forestgreen', inner="quartile")
     ax.set_xlabel("opening_tweet")
@@ -104,14 +104,13 @@ def violin_sentiment(conn, cursor):
     ax = fig.add_subplot(gs[1, 4])
     sns.violinplot(data=df4['last_tweet'], color='forestgreen', inner="quartile")
     ax.set_xlabel("last_tweet")
-
+    ax.set_title('Lufthansa')
     ax = fig.add_subplot(gs[1, 5])
     sns.violinplot(data=df4['delta_sentiment'], color='forestgreen', inner="quartile")
     ax.set_xlabel("delta_sentiment")
-
+    plt.suptitle('Distribution of sentiment in conversations for 4 airlines')
     fig.tight_layout()
-
-    plt.show()
+    plt.savefig("Plots\\violin.png", format='png')
 
 
 # instant
@@ -139,11 +138,13 @@ def bar_box_count(conn, cursor):
 
     #make the plot
     fig, ax = plt.subplots()
+    fig.figure(dpi=1200)
     ax.bar(range(1, len(data_len)+1), [length_KLM, length_BA, length_LH, length_RA], color='lightblue', align='center')
     ax2 = ax.twinx()
     ax2.boxplot(example_data)
     ax2.set_ylim(0, 15)
     ax.set_xticklabels(labels) # , rotation='vertical')
+    plt.savefig("Plots\\bar_box_count.png", format='png')
     plt.show()
 
 
@@ -154,7 +155,7 @@ def sentiment_year(conn, cursor):
                           "AND sentiment is not NULL "
                           "union select main.created_at, head_sentiment from KLM inner join main on id_str = head", conn)
         df1['created_at'] = pd.to_datetime(df1['created_at'])
-        fig, ax1 = plt.subplots(figsize=(16, 8))
+        fig, ax1 = plt.subplots(figsize=(16, 8), dpi=1200)
         ax1.set_xlabel('Date')
         ax1.set_ylabel('Sentiment (0 is negative, 1 is positive)')
         df1 = df1.rename(columns={"created_at": "Date"})
@@ -171,6 +172,7 @@ def sentiment_year(conn, cursor):
         ax2.xaxis.set_major_formatter(mdates.DateFormatter("%b-'%y"))
         plt.title("Average sentiment and opening tweets per day for KLM (NOTE: follow-up tweets not included)")
         plt.tight_layout()
+        plt.savefig("Plots\\sentiment_year_KLM.png", format='png')
         plt.show()
     if 1:
         df2 = pd.read_sql("select created_at, sentiment from main where user_mentions like '%British_Airways%' "
@@ -179,7 +181,7 @@ def sentiment_year(conn, cursor):
                           " inner join main on id_str = head", conn)
         df2['created_at'] = pd.to_datetime(df2['created_at'])
         df2 = df2.rename(columns={"created_at": "Date"})
-        fig, ax1 = plt.subplots(figsize=(16, 8))
+        fig, ax1 = plt.subplots(figsize=(16, 8), dpi=1200)
         ax1.set_ylabel('Sentiment (0 is negative, 1 is positive)')
         df2.groupby([df2['Date'].dt.date])['sentiment'].mean().plot(kind='line')
         plt.xticks(rotation=45)
@@ -194,6 +196,7 @@ def sentiment_year(conn, cursor):
         ax2.xaxis.set_major_formatter(mdates.DateFormatter("%b-'%y"))
         plt.title("Average sentiment and opening tweets per day for British Airways (NOTE: follow-up tweets not included)")
         plt.tight_layout()
+        plt.savefig("Plots\\sentiment_year_BA.png", format='png')
         plt.show()
     if 1:
         df3 = pd.read_sql("select created_at, sentiment from main where user_mentions like '%RyanAir%' "
@@ -201,7 +204,7 @@ def sentiment_year(conn, cursor):
                           "union select main.created_at, head_sentiment from RyanAir inner join main on id_str = head", conn)
         df3['created_at'] = pd.to_datetime(df3['created_at'])
         df3 = df3.rename(columns={"created_at": "Date"})
-        fig, ax1 = plt.subplots(figsize=(16, 8))
+        fig, ax1 = plt.subplots(figsize=(16, 8), dpi=1200)
         ax1.set_ylabel('Sentiment (0 is negative, 1 is positive)')
         df3.groupby([df3['Date'].dt.date])['sentiment'].mean().plot(kind='line')
         plt.xticks(rotation=45)
@@ -216,6 +219,7 @@ def sentiment_year(conn, cursor):
         ax2.xaxis.set_major_formatter(mdates.DateFormatter("%b-'%y"))
         plt.title("Average sentiment and opening tweets per day for RyanAir (NOTE: follow-up tweets not included)")
         plt.tight_layout()
+        plt.savefig("Plots\\sentiment_year_RyanAir.png", format='png')
         plt.show()
     if 1:
         df4 = pd.read_sql("select created_at, sentiment from main where user_mentions like '%Lufthansa%' "
@@ -224,7 +228,7 @@ def sentiment_year(conn, cursor):
                           " inner join main on id_str = head", conn)
         df4['created_at'] = pd.to_datetime(df4['created_at'])
         df4 = df4.rename(columns={"created_at": "Date"})
-        fig, ax1 = plt.subplots(figsize=(16, 8))
+        fig, ax1 = plt.subplots(figsize=(16, 8), dpi=1200)
         ax1.set_ylabel('Sentiment (0 is negative, 1 is positive)')
         df4.groupby([df4['Date'].dt.date])['sentiment'].mean().plot(kind='line')
         plt.xticks(rotation=45)
@@ -239,4 +243,5 @@ def sentiment_year(conn, cursor):
         ax2.xaxis.set_major_formatter(mdates.DateFormatter("%b-'%y"))
         plt.title("Average sentiment and opening tweets per day for Lufthansa (NOTE: follow-up tweets not included)")
         plt.tight_layout()
+        plt.savefig("Plots\\sentiment_year_Luft.png", format='png')
         plt.show()
